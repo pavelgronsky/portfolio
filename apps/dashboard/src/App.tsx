8e5d8978cc33294a9ff696ea9ca0f78b3fe36835
Pawel Gronski <pawel.gronski@pronos.eu>
2024-03-03 11:36:11 +0100

    Add tailwind

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => (
     <div className="App">
-        Test
+        <div className='w-full h-screen text-4xl text-blue-700 flex items-center justify-center'>
+            <h1 className='text-6xl text-red-500'>Test</h1>
+        </div>
     </div>
 )
 
